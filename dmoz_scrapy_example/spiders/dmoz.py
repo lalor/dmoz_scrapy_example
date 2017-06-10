b4ac9ecbec8a4ec98091589d98737c353922d3bc
Mingxing LAI <me@mingxinglai.com>
2017-06-10 19:54:07 +0800

    using xpath to parse data

--- dmoz_scrapy_example/spiders/dmoz.py
+++ dmoz_scrapy_example/spiders/dmoz.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import scrapy
 
 
@@ -11,4 +12,8 @@ class DmozSpider(scrapy.Spider):
     def parse(self, response):
         filename = response.url.split("/")[-2]
         with open(filename, 'wb') as f:
-            f.write(response.body)
+            for index, item in enumerate(response.xpath('//div[@class="title-and-desc"]'), 1):
+                title = item.xpath('./a/div/text()').extract()[0] # 标题
+                link = item.xpath('./a/@href').extract()[0] # 链接
+                desc = item.xpath('./div/text()').extract()[0].strip() # 详细描述
+                print(index, title, link, desc, sep=',', file=f)
