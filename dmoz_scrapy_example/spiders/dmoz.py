cce41cf677fa70cdd1ba164fa67691c9b2aa4ad2
Mingxing LAI <me@mingxinglai.com>
2017-06-10 19:49:17 +0800

    crawl web page

--- dmoz_scrapy_example/spiders/dmoz.py
+++ dmoz_scrapy_example/spiders/dmoz.py
@@ -5,7 +5,10 @@ import scrapy
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoztools.net"]
-    start_urls = ['http://dmoztools.net/']
+    start_urls = ['http://dmoztools.net/Computers/Programming/Languages/Python/Resources/',
+                    'http://dmoztools.net/Computers/Programming/Languages/Python/Books/']
 
     def parse(self, response):
-        pass
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
